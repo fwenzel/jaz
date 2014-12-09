0bd98eb08652fa93407c11893c8a013b84115760
Fred Wenzel <fwenzel@mozilla.com>
2014-12-08 21:42:26 -0800

    Make all animation info dependent on "phase"

--- src/animations/blub.js
+++ src/animations/blub.js
@@ -1,17 +1,18 @@
-import {rgba, hsla} from '../util.js';
-import {phaseMod} from '../cycles.js';
+import {sinNorm, cosNorm} from '../cycles.js';
 
 export class Blub {
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
 
-    this.radius = 0;  // Smallest ring size
     this.ringMargin = 30;  // Distance between rings.
-    this.maxradius = Math.sqrt(Math.pow(this.canvas.width, 2) + Math.pow(this.canvas.height, 2)) + 1;  // Largest ring we'll draw is just barely outside the canvas.
+    // Largest ring we'll draw is just barely outside the canvas.
+    this.maxRadius = (Math.sqrt(Math.pow(this.canvas.width, 2)
+                      + Math.pow(this.canvas.height, 2)) + 1);
   }
 
   tick(phase) {
+    // Move center.
     var x = this.canvas.width / 2;
     var y = this.canvas.height / 2;
 
@@ -20,15 +21,12 @@ export class Blub {
     this.ctx.lineWidth = 3;
 
     // Draw all rings.
-    for (var radius = this.radius; radius < this.maxradius; radius += this.ringMargin) {
-      // Draw ring.
+    var ringRep = 500 * phase % this.ringMargin;
+    for (var radius = ringRep; radius < this.maxRadius; radius += this.ringMargin) {
       this.ctx.beginPath();
       this.ctx.arc(x, y, radius, 0, Math.PI * 2);
       this.ctx.stroke();
     }
-
-    this.radius += 1.5;
-    this.radius %= this.ringMargin;
 
     this.ctx.restore();
   }
